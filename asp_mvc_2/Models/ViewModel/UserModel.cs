aa2f86dc187c55bd871d98c8c9853b6a5835671a
yeninurazizah <yeninurazizah14@gmail.com>
2017-02-09 20:19:01 +0700

    Add project files.

---
+++ asp_mvc_2/Models/ViewModel/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace asp_mvc_2.Models.ViewModel
+{
+    public class UserModel
+    {
+
+    }
+
+    public class UserLoginView
+    {
+        [Key]
+        public int SYSUserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Login ID")]
+        public string LoginName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+
+}
